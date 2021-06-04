99b37906c503b33cd2b92f872a2dbb9fcc559a1f
Helix088 <pat20001@byui.edu>
2021-06-03 23:04:10 -0500

    created comparison method

--- speed/game/compare.py
+++ speed/game/compare.py
@@ -1,24 +1,20 @@
-from word.py import Word
-from buffer.py import Buffer
-
-
 class Compare:
     """ Service Provider - Compares what is typed in Buffer with word list from words.py
-
-        Attributes:
-
     """
 
     def __init__(self):
         super().__init__()
 
-    def compare(self, words, buffer):
-        """ compare_words(Param - words, bufferthing) - Returns index if there is a match otherwise return 9999999
+    def comparison(self, words, buffer):
+        """ Determines if the input (Buffer) matches the words (Word) on screen
 
             Args:
               self (Compare): an instance of Compare.
-              words (Word): an instance of Word.
-              buffer (Buffer): an instance of Buffer.
 
             Returns - index of matched word
         """
+        for word in words:
+            if word == buffer:
+                return word
+            else:
+                return "9999999"
