41f58dae7f18b8be40d2d961d65b27c057f33dd4
drbubbles40-school <82907102+drbubbles40-school@users.noreply.github.com>
2021-06-05 13:50:05 -0600

    Fixed backspace error, added additional word fun

--- speed/game/constants.py
+++ speed/game/constants.py
@@ -5,6 +5,6 @@ MAX_Y = 20
 FRAME_LENGTH = 0.08
 STARTING_WORDS = 5
 MAX_WORDS = MAX_Y - 2 #Tracks Max number of words that can be on screen
-DIFFICULTY = 5
+DIFFICULTY = 2
 PATH = os.path.dirname(os.path.abspath(__file__))
 LIBRARY = open(PATH + "/words.txt").read().splitlines()
